50729a897b8c1155cfbfe717ebe1d2f46ee72606
Jeff Garris <jgarris@ignitexds.com>
2015-01-20 16:50:41 -0500

    Working on Responsive functionality

--- wp-content/themes/my-simone/functions.php
+++ wp-content/themes/my-simone/functions.php
@@ -86,6 +86,12 @@ add_action( 'widgets_init', 'my_simone_widgets_init' );
  */
 function my_simone_scripts() {
 	wp_enqueue_style( 'my-simone-style', get_stylesheet_uri() );
+	
+	wp_enqueue_style( 'my-simone-google-fonts', 'http://fonts.googleapis.com/css?family=Lato:100,400,700,900,400italic,900italic|PT+Serif:400,700,400italic,700italic' );
+	
+	wp_enqueue_style( 'my-simone-fontawesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' );
+	
+	wp_enqueue_style('my-simone-content-sidebar', get_template_directory_uri() . '/layouts/content-sidebar.css');
 
 	wp_enqueue_script( 'my-simone-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
