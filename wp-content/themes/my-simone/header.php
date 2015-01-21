3567a12599d25f3cac0d63a1d0506b402ce26d9f
Jeff Garris <jgarris@ignitexds.com>
2015-01-21 15:56:11 -0500

    Styled header and nav

--- wp-content/themes/my-simone/header.php
+++ wp-content/themes/my-simone/header.php
@@ -22,9 +22,32 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		<?php if ( get_header_image() && ('blank' == get_header_textcolor()) ) : ?>
+			<div class="header-image">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+				</a>
+			</div>
+		<?php endif; // End header image check. ?>
+		
+		<?php if ( get_header_image() && ('blank' == get_header_textcolor()) ) { ?>
+		    <figure class="header-image">
+		        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		                <img src="<?php header_image(); ?>" width="< ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+		        </a>
+		    </figure>
+		<?php } // End header image check. ?>
+		<?php 
+		    if ( get_header_image() && !('blank' == get_header_textcolor()) ) { 
+		        echo '<div class="site-branding header-background-image" style="background-image: url(' . get_header_image() . ')">'; 
+		    } else {
+		        echo '<div class="site-branding">';
+		    }
+		?>
+			<div class="title-box">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</div>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
@@ -32,7 +55,10 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'my-simone' ); ?></a>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			<?php my_simone_social_menu(); ?>
 		</nav><!-- #site-navigation -->
+		
+		<?php get_search_form(); ?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
